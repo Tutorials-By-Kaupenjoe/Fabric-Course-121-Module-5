5bc3d2c2f95e70a3049138caba748542cefb49c5
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-07-17 15:58:10 +0200

    custom commands

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -2,10 +2,16 @@ package net.kaupenjoe.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.kaupenjoe.mccourse.command.ReturnHomeCommand;
+import net.kaupenjoe.mccourse.command.SetHomeCommand;
+import net.kaupenjoe.mccourse.event.PlayerCopyHandler;
 import net.kaupenjoe.mccourse.item.ModItemGroups;
 import net.kaupenjoe.mccourse.item.ModItems;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,5 +27,9 @@ public class MCCourseMod implements ModInitializer {
 		ModBlocks.registerModBlocks();
 
 		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
+
+		CommandRegistrationCallback.EVENT.register(SetHomeCommand::register);
+		CommandRegistrationCallback.EVENT.register(ReturnHomeCommand::register);
+		ServerPlayerEvents.COPY_FROM.register(new PlayerCopyHandler());
 	}
 }
---
+++ src/main/java/net/kaupenjoe/mccourse/command/SetHomeCommand.java
@@ -0,0 +1,31 @@
+package net.kaupenjoe.mccourse.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.kaupenjoe.mccourse.util.IEntityDataSaver;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+
+public class SetHomeCommand {
+    public static void register(CommandDispatcher<ServerCommandSource> serverCommandSourceCommandDispatcher,
+                                CommandRegistryAccess commandRegistryAccess,
+                                CommandManager.RegistrationEnvironment registrationEnvironment) {
+        serverCommandSourceCommandDispatcher.register(CommandManager.literal("home")
+                .then(CommandManager.literal("set").executes(SetHomeCommand::run)));
+    }
+
+    private static int run(CommandContext<ServerCommandSource> context) {
+        IEntityDataSaver player = ((IEntityDataSaver) context.getSource().getPlayer());
+        BlockPos playerPos = context.getSource().getPlayer().getBlockPos();
+        String positionString = "(" + playerPos.getX() + ", " + playerPos.getY() + ", " + playerPos.getZ() + ")";
+
+        player.getPersistentData().putIntArray("mccourse.homepos",
+                new int[] { playerPos.getX(), playerPos.getY(), playerPos.getZ() });
+
+        context.getSource().sendFeedback(() -> Text.literal("Set Home at " + positionString), true);
+        return 1;
+    }
+}
