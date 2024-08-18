ed2b74e05e4a3e3621c5cc7afac1858b7f6e3563
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-08-18 14:11:33 +0200

    spawn particles

--- src/main/java/net/kaupenjoe/mccourse/item/custom/ChainsawItem.java
+++ src/main/java/net/kaupenjoe/mccourse/item/custom/ChainsawItem.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -35,6 +36,10 @@ public class ChainsawItem extends Item {
                         Objects.requireNonNull(context.getPlayer()).sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
 
                 context.getWorld().playSound(null, context.getBlockPos(), ModSounds.CHAINSAW_CUT, SoundCategory.BLOCKS, 1f, 1f);
+
+                // Server Particles (Via Server, Seen by all players)
+                ((ServerWorld) context.getWorld()).spawnParticles(ParticleTypes.SCULK_SOUL, context.getBlockPos().getX() + 0.5f, context.getBlockPos().getY() + 1.0f,
+                        context.getBlockPos().getZ() + 0.5f, 25, 0.0, 0.05, 0.0, 0.15f);
             } else {
                 context.getWorld().playSound(null, context.getBlockPos(), ModSounds.CHAINSAW_PULL, SoundCategory.BLOCKS, 1f, 1f);
             }
